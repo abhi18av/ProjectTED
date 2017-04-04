4c46d763c5062fde3262839670ca7b7524e3b49e
Abhinav Sharma <abhi18av@gmail.com>
2017-04-04 11:42:13 +0530

    GoTed is almost complete for the talk_main

--- TED_main.go
+++ TED_main.go
@@ -26,37 +26,47 @@ func main() {
 	doc, err := goquery.NewDocument(url)
 	check(err)
 
-	//speaker := doc.Find(".talk-speaker__name").Contents().Text()
-	//fmt.Println(speaker)
+	speaker := doc.Find(".talk-speaker__name").Contents().Text()
+	fmt.Println(speaker)
 
-	//title := doc.Find(".player-hero__title__content").Contents().Text()
-	//fmt.Println(title)
+	title := doc.Find(".player-hero__title__content").Contents().Text()
+	fmt.Println(title)
 
 	duration := doc.Find(".player-hero__meta").Contents().Text()
 	//fmt.Println(duration)
 
-	/*
-		for _, x := range strings.Split(duration, "\n") {
-			fmt.Println(x)
-			println("~~~~~~")
-		}
-	*/
+	//for _, x := range strings.Split(duration, "\n") {
+	//	fmt.Println(x)
+	//	println("~~~~~~")
+	//}
+
 	x := strings.Split(duration, "\n")
 	fmt.Println(x[6])
 
-	/*
-		time_filmed := doc.Find(".player-hero__meta__label").Contents().Text()
+	time_filmed := doc.Find(".player-hero__meta").Contents().Text()
 
-		fmt.Println(time_filmed)
+	//	fmt.Println(time_filmed)
 
-		talk_views_count := doc.Find("#sharing-count").Contents().Text()
-		fmt.Println(talk_views_count)
+	y := strings.Split(time_filmed, "\n")
+	fmt.Println(y[11])
 
-		talk_topics := doc.Find(".talk-topics__list").Contents().Text()
-		fmt.Println(talk_topics)
+	talk_views_count := doc.Find("#sharing-count").Contents().Text()
+	//	fmt.Println(talk_views_count)
 
-		talk_comments_count := doc.Find(".h11").Contents().Text()
-		fmt.Println(talk_comments_count)
+	a := strings.Split(talk_views_count, "\n")
+	b := strings.TrimSpace(a[2])
+	fmt.Println(b)
 
-	*/
+	talk_topics := doc.Find(".talk-topics__list").Contents().Text()
+	//fmt.Println(talk_topics)
+
+	c := strings.Split(talk_topics, "\n")
+
+	fmt.Println(c[3:len(c)])
+
+	talk_comments_count := doc.Find(".h11").Contents().Text()
+	//fmt.Println(talk_comments_count)
+	d := strings.Split(talk_comments_count, " ")
+	fmt.Println(d[0])
+
 }
