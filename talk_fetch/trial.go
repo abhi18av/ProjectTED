bceee8728747c3aae0e7629f9ce78d07fd22f8c6
Abhinav Sharma <abhi18av@gmail.com>
2017-04-16 10:26:23 +0530

    refactored the trials file for a minimal use-case

--- talk_fetch/trial.go
+++ talk_fetch/trial.go
@@ -2,124 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var langCodes = map[string]string{
-	"Afrikaans":             "af",
-	"Albanian":              "sq",
-	"Algerian Arabic":       "arq",
-	"Amharic":               "am",
-	"Arabic":                "ar",
-	"Armenian":              "hy",
-	"Assamese":              "as",
-	"Asturian":              "ast",
-	"Azerbaijani":           "az",
-	"Basque":                "eu",
-	"Belarusian":            "be",
-	"Bengali":               "bn",
-	"Bislama":               "bi",
-	"Bosnian":               "bs",
-	"Bulgarian":             "bg",
-	"Burmese":               "my",
-	"Catalan":               "ca",
-	"Cebuano":               "ceb",
-	"Chinese, Simplified":   "zh-cn",
-	"Chinese, Traditional":  "zh-tw",
-	"Chinese, Yue":          "zh",
-	"Creole, Haitian":       "ht",
-	"Croatian":              "hr",
-	"Czech":                 "cs",
-	"Danish":                "da",
-	"Dutch":                 "nl",
-	"Dzongkha":              "dz",
-	"English":               "en",
-	"Esperanto":             "eo",
-	"Estonian":              "et",
-	"Filipino":              "fil",
-	"Finnish":               "fi",
-	"French":                "fr",
-	"French (Canada)":       "fr-ca",
-	"Galician":              "gl",
-	"Georgian":              "ka",
-	"German":                "de",
-	"Greek":                 "el",
-	"Gujarati":              "gu",
-	"Hakha Chin":            "cnh",
-	"Hausa":                 "ha",
-	"Hebrew":                "he",
-	"Hindi":                 "hi",
-	"Hungarian":             "hu",
-	"Hupa":                  "hup",
-	"Icelandic":             "is",
-	"Igbo":                  "ig",
-	"Indonesian":            "id",
-	"Ingush":                "inh",
-	"Irish":                 "ga",
-	"Italian":               "it",
-	"Japanese":              "ja",
-	"Kannada":               "kn",
-	"Kazakh":                "kk",
-	"Khmer":                 "km",
-	"Klingon":               "tlh",
-	"Korean":                "ko",
-	"Kurdish":               "ku",
-	"Kyrgyz":                "ky",
-	"Lao":                   "lo",
-	"Latgalian":             "ltg",
-	"Latin":                 "la",
-	"Latvian":               "lv",
-	"Lithuanian":            "lt",
-	"Luxembourgish":         "lb",
-	"Macedo":                "rup",
-	"Macedonian":            "mk",
-	"Malagasy":              "mg",
-	"Malay":                 "ms",
-	"Malayalam":             "ml",
-	"Maltese":               "mt",
-	"Marathi":               "mr",
-	"Mauritian Creole":      "mfe",
-	"Mongolian":             "mn",
-	"Montenegrin":           "srp",
-	"Nepali":                "ne",
-	"Norwegian Bokmal":      "nb",
-	"Norwegian Nynorsk":     "nn",
-	"Occitan":               "oc",
-	"Pashto":                "ps",
-	"Persian":               "fa",
-	"Polish":                "pl",
-	"Portuguese":            "pt",
-	"Portuguese, Brazilian": "pt-br",
-	"Punjabi":               "pa",
-	"Romanian":              "ro",
-	"Russian":               "ru",
-	"Rusyn":                 "ry",
-	"Serbian":               "sr",
-	"Serbo-Croatian":        "sh",
-	"Silesian":              "szl",
-	"Sinhala":               "si",
-	"Slovak":                "sk",
-	"Slovenian":             "sl",
-	"Somali":                "so",
-	"Spanish":               "es",
-	"Swahili":               "sw",
-	"Swedish":               "sv",
-	"Swedish Chef":          "art-x-bork",
-	"Tagalog":               "tl",
-	"Tajik":                 "tg",
-	"Tamil":                 "ta",
-	"Tatar":                 "tt",
-	"Telugu":                "te",
-	"Thai":                  "th",
-	"Tibetan":               "bo",
-	"Turkish":               "tr",
-	"Turkmen":               "tk",
-	"Ukrainian":             "uk",
-	"Urdu":                  "ur",
-	"Uyghur":                "ug",
-	"Uzbek":                 "uz",
-	"Vietnamese":            "vi",
+	"Chinese, Simplified": "zh-cn",
+	"English":             "en",
+	"German":              "de",
+	"Russian":             "ru",
 }
 
 // this should return an array of strings => ["langs"]
@@ -148,23 +40,9 @@ func main() {
 	//url := "https://www.ted.com/talks/shawn_achor_the_happy_secret_to_better_work/transcript?language=en"
 	//url := "https://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action/transcript?language=en"
 
-	/*
-		var url string
-
-		fmt.Scanf("%s", &url)
-
-	*/
-
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		panic(err)
 	}
-
-	//	posted(doc)
-	//	rated(doc)
-	//	local_title(doc)
-
 	fmt.Println(len(langs(doc)))
-	//	times(doc)
-	//	texts(doc)
 }
