cde654816504246f588f5cc4c14472d4ce6fdab7
Abhinav Sharma <abhi18av@gmail.com>
2017-05-13 23:26:44 +0530

    I've come up with a simple thread game for understanding concurrency

--- experiments/Scratch/scratch.go
+++ experiments/Scratch/scratch.go
@@ -1,16 +1,25 @@
+// Have a counting of << 1 to 10 >> in each thread.
 package main
 
 import "fmt"
 
 func main() {
 
-	for i := 1; i < 10; i++ {
-		x := sqr(i)
-		fmt.Println(i, " : ", x)
+	var threadNames = [...]string{"T1", "T2", "T3", "T4", "T5"}
+	// Execute this on 5 diference threads
+	for _, thread := range threadNames {
+		oneToTen(thread)
 	}
-
 }
 
-func sqr(x int) int {
-	return x * x
+func oneToTen(channelName string) {
+
+	fmt.Println("\n\n # ", channelName, " #")
+
+	fmt.Println(">>>>>>>>>>>>>>>")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("<<<<<<<<<<<<<<<")
 }
