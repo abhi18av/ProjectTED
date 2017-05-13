c01d132dcac9df3e09613b3d6ead3ae2af0fcd6c
Abhinav Sharma <abhi18av@gmail.com>
2017-05-13 05:42:50 +0530

    Yup, got a basic version working

--- experiments/async/async.go
+++ experiments/async/async.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -16,12 +16,20 @@ func main() {
 		"https://www.ted.com/talks/ken_robinson_says_schools_kill_creativity/transcript?language=ru",
 	}
 
-	ch := make(chan string)
+	ch := make(chan []string)
 
 	for _, url := range urls {
 
-		go fetch(url, ch)
+		go talk_texts(url, ch)
 	}
+
+	/*
+		for _, url := range urls {
+
+			go fetch(url, ch)
+		}
+
+	*/
 
 	for range urls {
 		fmt.Println(<-ch)
@@ -29,6 +37,7 @@ func main() {
 
 }
 
+/*
 func fetch(url string, ch chan<- string) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -46,4 +55,19 @@ func fetch(url string, ch chan<- string) {
 	//	ch <- fmt.Sprintf("%7d    %s", nbytes, url)
 
 	ch <- fmt.Println(resp.Body)
+}
+*/
+
+func talk_texts(url string, ch chan<- []string) {
+	page, _ := goquery.NewDocument(url)
+
+	texts := page.Find(".talk-transcript__para__text").Contents().Text()
+	var para []string
+	for _, text := range strings.Split(texts, "\n\n") {
+
+		//fmt.Println(text)
+		para = append(para, text)
+	}
+
+	ch <- para
 }
