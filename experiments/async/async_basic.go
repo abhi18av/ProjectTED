e0cc435e44e34294c29119c165a7b6a00e81dc3a
Abhinav Sharma <abhi18av@gmail.com>
2017-05-16 00:17:14 +0530

    one2ten has been used for using the multidimensional array

--- experiments/async/async_basic.go
+++ experiments/async/async_basic.go
@@ -16,7 +16,7 @@ func main() {
 	wg.Add(len(threadNames))
 
 	type collections struct {
-		title []string
+		title [][]int
 	}
 
 	var talk collections
@@ -34,20 +34,22 @@ func main() {
 	fmt.Println(talk)
 } // end of main()
 
-func currentThread(aThread string) string {
+func currentThread(aThread string) []int {
 
 	// Sleep for arbitrary amount of time
 	time.Sleep(time.Millisecond * time.Duration(rand.Intn(10000)))
 
 	// Line seperation for better formatting
 	// fmt.Println("# ", aThread, " #")
-	return (aThread)
-	/*
-		// Prints 1 to 10 on the screen
-		fmt.Println(">>>>>>>>>>>>>>>")
-		for i := 1; i <= 10; i++ {
-			fmt.Println(i)
-		}
-		fmt.Println("<<<<<<<<<<<<<<<")
-	*/
+
+	var one2ten []int
+
+	// Prints 1 to 10 on the screen
+	//fmt.Println(">>>>>>>>>>>>>>>")
+	for i := 1; i <= 10; i++ {
+		one2ten = append(one2ten, i)
+	}
+	//fmt.Println("<<<<<<<<<<<<<<<")
+
+	return one2ten
 }
