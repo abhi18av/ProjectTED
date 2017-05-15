cf6666fe9930e433196d44cdc89358d4f3019408
Abhinav Sharma <abhi18av@gmail.com>
2017-05-16 00:01:05 +0530

    working on the async writing on struct

--- experiments/async/async_basic.go
+++ experiments/async/async_basic.go
@@ -16,7 +16,7 @@ func main() {
 	wg.Add(len(threadNames))
 
 	type collections struct {
-		title string
+		title []string
 	}
 
 	var talk collections
@@ -25,20 +25,22 @@ func main() {
 	for _, thread := range threadNames {
 		go func(thread string) {
 			defer wg.Done()
-			talk.title = currentThread(thread)
+			talk.title = append(talk.title, currentThread(thread))
 		}(thread)
 	}
 
 	wg.Wait()
+
+	fmt.Println(talk)
 } // end of main()
 
 func currentThread(aThread string) string {
 
 	// Sleep for arbitrary amount of time
-	time.Sleep(time.Millisecond * time.Duration(rand.Intn(5000)))
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(10000)))
 
 	// Line seperation for better formatting
-	fmt.Println("# ", aThread, " #")
+	// fmt.Println("# ", aThread, " #")
 	return (aThread)
 	/*
 		// Prints 1 to 10 on the screen
