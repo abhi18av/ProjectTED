d9752d549ba6cf036e4f9af2d1facd97946ca87b
Abhinav Sharma <abhi18av@gmail.com>
2017-05-16 16:56:47 +0530

    learning about channels

--- experiments/async/channels.go
+++ experiments/async/channels.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -12,26 +13,25 @@ func main() {
 	go func() {
 		for {
 			c1 <- "from 1"
-			time.Sleep(time.Second * 2)
+			time.Sleep(timeSeed())
 		}
 	}()
 
 	go func() {
 		for {
 			c2 <- "from 2"
-			time.Sleep(time.Second * 3)
+			time.Sleep(timeSeed())
 		}
 	}()
 
-	go func() {
-		for {
-			select {
-			case msg1 := <-c1:
-				fmt.Println(msg1)
-			case msg2 := <-c2:
-				fmt.Println(msg2)
-			}
-		}
-	}()
+	msg1 := <-c1
+	fmt.Println(msg1)
+	msg2 := <-c2
+	fmt.Println(msg2)
+}
 
+func timeSeed() time.Duration {
+
+	duration := time.Millisecond * time.Duration(rand.Intn(1000))
+	return duration
 }
