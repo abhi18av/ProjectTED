860b798cc2c42f7befd8d5d02831f41f7c999742
Abhinav Sharma <abhi18av@gmail.com>
2017-05-16 18:15:15 +0530

    used the select statement to recieve values from channels

--- experiments/async/channels.go
+++ experiments/async/channels.go
@@ -10,28 +10,37 @@ func main() {
 	c1 := make(chan string)
 	c2 := make(chan string)
 
-	go func() {
-		for {
-			c1 <- "from 1"
-			time.Sleep(timeSeed())
-		}
-	}()
+	go func1(c1)
+	go func2(c2)
 
-	go func() {
-		for {
-			c2 <- "from 2"
-			time.Sleep(timeSeed())
-		}
-	}()
+	var msg1 string
+	var msg2 string
 
-	msg1 := <-c1
-	fmt.Println(msg1)
-	msg2 := <-c2
-	fmt.Println(msg2)
+	select {
+	case msg1 = <-c1:
+		fmt.Println(msg1)
+	case msg2 = <-c2:
+		fmt.Println(msg2)
+	}
+
+	fmt.Println("Before Exit")
+	fmt.Println(msg1, msg2)
 }
 
 func timeSeed() time.Duration {
 
-	duration := time.Millisecond * time.Duration(rand.Intn(1000))
+	duration := time.Millisecond * time.Duration(rand.Intn(10000))
 	return duration
+}
+
+func func1(ch chan string) {
+	ch <- "from 1"
+	time.Sleep(timeSeed())
+
+}
+
+func func2(ch chan string) {
+	ch <- "from 2"
+	time.Sleep(timeSeed())
+
 }
