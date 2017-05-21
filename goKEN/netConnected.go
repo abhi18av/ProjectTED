8b63e0e6a2d5574cfee824fb54a330f52feff297
Abhinav Sharma <abhi18av@gmail.com>
2017-05-21 09:38:17 +0530

    added a list of golang libs I wish to try out

--- goKEN/netConnected.go
+++ goKEN/netConnected.go
@@ -9,32 +9,11 @@ import (
 )
 
 func main() {
-
-	/*
-				"github.com/gizak/termui"
-				"github.com/spf13/cobra"
-				"github.com/fatih/color"
-				"github.com/gosuri/uiprogress"
-				"github.com/briandowns/spinner"
-		https://github.com/HouzuoGuo/tiedot
-		https://github.com/boltdb/bolt
-		https://github.com/cznic/ql
-		https://github.com/patrickmn/go-cache
-		https://github.com/syndtr/goleveldb
-		https://github.com/cayleygraph/cayley
-		https://github.com/dgraph-io/dgraph
-		https://github.com/mattn/go-sqlite3
-		https://github.com/gdamore/tcell
-		https://github.com/kyokomi/emoji
-		go get github.com/gonum/plot/...
-		go get -u github.com/zieckey/gochart
-
-	*/
 	// Make a get request
 	rs, err := http.Get("https://google.com")
 	// Process response
 	if err != nil {
-		color.Red("Not connected")
+		color.Red("WiFI OFF")
 		//panic("Not connected to the net") // More idiomatic way would be to print the error and die unless it's a serious error
 
 		// Learn about exit status in Golang
