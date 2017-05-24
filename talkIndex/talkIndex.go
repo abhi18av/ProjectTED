c812cf67745c54675e80e3015b1a8e73c19fc958
Abhinav Sharma <abhi18av@gmail.com>
2017-05-24 11:00:27 +0530

    Yup - making quick progress now

--- talkIndex/talkIndex.go
+++ talkIndex/talkIndex.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 
-	startingIndexURL := "https://www.ted.com/talks?page=1"
+	startingIndexURL := "https://www.ted.com/talks?page=2"
 	aPage, _ := goquery.NewDocument(startingIndexURL)
 	collectTalkLinks(aPage)
 }
 
-func collectTalkLinks(doc *goquery.Document) {
-	urls, _ := doc.Find(".ga-link").Attr("href")
-	fmt.Println(urls)
-	//return urls
+func collectTalkLinks(doc *goquery.Document) []string {
+	var urls []string
+
+	doc.Find(".h9").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Find("a").Attr("href")
+		//fmt.Println(i+1, link)
+		urls = append(urls, link)
+	})
+
+	//fmt.Println(urls)
+	return urls
 }
