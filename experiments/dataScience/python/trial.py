018f3d5e3d51b4d4a9eb1f80406b92a6aa879e49
Abhinav Sharma <abhi18av@gmail.com>
2017-05-26 15:57:41 +0530

    re-brushing my python skills

--- experiments/dataScience/python/trial.py
+++ experiments/dataScience/python/trial.py
@@ -1,6 +1,9 @@
-import os
+import random
+min = 1
+max = 100
 
+roll_again = "yes"
 
-
-for i in range(10):
-                print(i)
+for i in range(9):
+    print(random.randint(min, max))
+    print(random.randint(min, max))
