e1e0969d70ed3ba55a5bd1201a24ffe0350fcf93
Abhinav Sharma <abhi18av@gmail.com>
2017-06-09 23:28:21 +0530

    working on a transcript page DF

---
+++ experiments/dataScience/Exp_3/julia/talkTranscriptDF.jl
@@ -0,0 +1,55 @@
+# AIM : Create a dataFrame from the information held by talkVideoPage in the json
+
+cd("/Users/eklavya/Projects/Polyglot/ProjectTED/experiments/dataScience/Exp_3/julia")
+
+
+using JSON
+using CSV
+using DataFrames
+
+file = "./laura_robinson_the_secrets_i_find_on_the_mysterious_ocean_floor.json"
+
+
+_contents = JSON.Parser.parsefile(file)
+transcriptPage = _contents["TalkTranscriptPage"]
+
+keys = collect(keys(transcriptPage))
+
+#println(keys)
+
+
+
+ videoPageDF = DataFrame( VideoURL = String[], #1
+                          Speaker = String[],#2
+                          AvailableSubtitlesCount = String[],#3
+                          Duraton = String[],#4
+                          #TalkTopicsList = String[], #5
+                          TimeFilmed = String[], #6
+                          TalkViewsCount = String[], #7
+                          TalkCommentsCount = String[] ) #8
+
+
+
+
+#df = DataFrame(a = String[], b= String[])
+#push!(df, ["a","b"])
+
+
+
+
+push!(videoPageDF,
+[videoPage[keys[1]],
+videoPage[keys[2]],
+videoPage[keys[3]],
+videoPage[keys[4]],
+#videoPage[keys[5]],
+videoPage[keys[6]],
+videoPage[keys[7]],
+videoPage[keys[8]]])
+
+
+
+#describe(videoPageDF)
+#size(videoPageDF)
+
+CSV.write("DF.csv", videoPageDF; delim= ';')
