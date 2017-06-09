765cf6d85847b47dcda28b9eda5cea2484d1f24f
Abhinav Sharma <abhi18av@gmail.com>
2017-06-09 13:03:49 +0530

    understood how to use for in rust

--- experiments/dataScience/SCRATCH/scratch_rust/src/main.rs
+++ experiments/dataScience/SCRATCH/scratch_rust/src/main.rs
@@ -1,15 +1,8 @@
 fn main() {
 
-    let mut x = 5; // mut x: i32
-    let mut done = false; // mut done: bool
 
-    while !done {
-        x += x - 3;
-
-        println!("{}", x);
-
-        if x % 5 == 0 {
-            done = true;
-        }
+    for x in 0..10 {
+        println!("{}", x); // x: i32
     }
+
 }
