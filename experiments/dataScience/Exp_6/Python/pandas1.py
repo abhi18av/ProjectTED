2c7159f4c78f26722cbf6ac875ba67c7d1bf8691
Abhinav Sharma <abhi18av@gmail.com>
2017-06-14 16:11:00 +0530

    I need to be able to read in the data from json properly

--- experiments/dataScience/Exp_6/Python/pandas1.py
+++ experiments/dataScience/Exp_6/Python/pandas1.py
@@ -1,5 +1,5 @@
 import json
-import pandas
+import pandas as pd
 import os
 import glob
 
@@ -10,8 +10,17 @@ files = glob.glob(tedTalkDB + "*.json")
 
 filelist = []
 
-
+"""
 for f in files:
     content = json.load(open(f))
     title_en = content["TalkTranscriptPage"]["TalkTranscript"]["en"]["LocalTalkTitle"]
     print(title_en)
+"""
+
+
+f1 = tedTalkDB +  "shah_rukh_khan_thoughts_on_humanity_fame_and_love.json" 
+f2 = tedTalkDB +  "sarah_kay_how_many_lives_can_you_live.json" 
+
+df = pd.read_json(f1)
+
+df.append(pd.read_json(f2))
