fb847e78d38729aeda7965e1da7545a11034260c
Abhinav Sharma <abhi18av@gmail.com>
2017-07-03 07:06:21 +0530

    Initiated a metabase clojure project

---
+++ experiments/dataScience/metabase1/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to metabase1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ experiments/dataScience/metabase1/test/metabase1/core_test.clj
@@ -0,0 +1,7 @@
+(ns metabase1.core-test
+  (:require [clojure.test :refer :all]
+            [metabase1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
